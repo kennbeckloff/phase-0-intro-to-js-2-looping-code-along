fab264c7f482c4cdd649e874a849309f0463d9e6
kennbeckloff <kennbeckloff688@gmail.com>
2022-06-02 13:49:06 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const countDown =()=>{
+    let count = 10;
+    while(count > -1){
+        console.log(count --);
+    }
+}
+
+let messages = [];
+const writeCards = (names, eventName) => {
+    names.forEach((name) => {
+        messages.push(`Thank you, ${name}, for the wonderful ${eventName} gift!`); 
+    })
+    return messages;
+}
